18b6be214d4ce5982308f25b8689822dc721e3a6
jaychelcalope-png <jaychelcalope@gmail.com>
2025-12-08 16:38:42 +0800

    Model is not loaded

--- app/routes/detect.py
+++ app/routes/detect.py
@@ -13,11 +13,11 @@ bp = Blueprint('detect', __name__, url_prefix='/detect')
 # Load trained model once
 #MODEL = load_model('rice_model.h5')
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
 MODEL_PATH = os.path.join(BASE_DIR, "rice_model.h5")
 MODEL_PATH = os.path.abspath(MODEL_PATH)
 print("Loading model from:", MODEL_PATH)
+MODEL = load_model(MODEL_PATH)
+
 
 # Labels used in predictions
 LABELS = [
