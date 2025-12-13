d90c477dc38cabd144d0ce04574feaa14aa47912
jaychelcalope-png <jaychelcalope@gmail.com>
2025-12-13 17:45:46 +0800

    changing the App Logo

--- app/static/service-worker.js
+++ app/static/service-worker.js
@@ -3,8 +3,6 @@ self.addEventListener("install", event => {
     caches.open("v1").then(cache => {
       return cache.addAll([
         "/",
-        "/static/css/style.css",
-        "/static/js/app.js"
       ]);
     })
   );
